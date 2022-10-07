6fb9ce6703a7ce0b27b8dc1be7323534022477c6
EnriqueHavenstein <95871479+EnriqueHavenstein@users.noreply.github.com>
2022-10-07 17:54:50 +0200

    Update functions.py

--- functions.py
+++ functions.py
@@ -7,10 +7,20 @@ def my_function():
 my_function()
 
 
-def my_function(name):          # parameter or argument
+def my_function(name):                              # parameter or argument
     print(name + ' Surname')
 
 
 my_function('Tom')
 
 
+def my_function(*kids):
+    print('The youngest child is ' + kids[2])       # if the number of arguments is not defined, * and list
+
+my_function('Emil', 'Mark', 'Linus')
+
+
+def my_function(child3, child2, child1):
+  print("The youngest child is " + child3)
+
+my_function(child1 = "Emil", child2 = "Tobias", child3 = "Linus
